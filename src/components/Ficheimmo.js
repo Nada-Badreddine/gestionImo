cac8150a6a7438fe200d4c87141bc0a29e9f7d45
saber <saberfathallah7@gmail.com>
2021-04-05 09:01:58 +0200

    imo

--- src/components/Ficheimmo.js
+++ src/components/Ficheimmo.js
@@ -1,92 +1,110 @@
-import { useEffect, useState } from 'react'
-import { Table } from 'antd';
+import { useEffect, useState } from "react";
+import { Table } from "antd";
 
-import { getAllClients } from '../services/clientsServices';
+import { getAllImo, removeImo } from "../services/imoServices";
 
 const columns = [
-    {
-      title: 'Designation',
-      width: 100,
-      dataIndex: 'designation',
-      key: 'designation',
-      fixed: 'left',
-    },
-    {
-      title: 'Categorie',
-      width: 100,
-      dataIndex: 'categorie',
-      key: 'categorie',
-      fixed: 'left',
-    },
-    {
-      title: 'Compte Comptable',
-      width: 100,
-      dataIndex: 'compte Comptable',
-      key: 'compte Comptable',
-      fixed: 'left',
-    },
-    {
-      title: 'Duree',
-      width: 100,
-      dataIndex: 'duree',
-      key: 'duree',
-      fixed: 'left',
-    },
-    {
-      title: 'montant',
-      width: 100,
-      dataIndex: 'montant',
-      key: 'montant',
-      fixed: 'left',
-    },
-    {
-      title: 'Date mis en service',
-      width: 100,
-      dataIndex: 'date mis en service',
-      key: 'date mis en service',
-      fixed: 'left',
-    },
-    {
-      title: 'Date d acquisition',
-      width: 100,
-      dataIndex: 'Date d acquisition',
-      key: 'Date d acquisition',
-      fixed: 'left',
-    },
-    {
-      title: 'Type d amortissement',
-      width: 100,
-      dataIndex: 'Type d amortissement',
-      key: 'Type d amortissement',
-      fixed: 'left',
-    },
-    
-    {
-      title: 'Date d acquisition',
-      width: 100,
-      dataIndex: 'categorie',
-      key: 'categorie',
-      fixed: 'left',
-    },
-    {
-      title: 'Action',
-      key: 'operation',
-      fixed: 'right',
-      width: 100,
-      render: () => <a>3amar bzwer</a>,
-    },
-  ];
+  {
+    title: "Designation",
+    width: 100,
+    dataIndex: "designation",
+    key: "designation",
+    fixed: "left",
+  },
+  {
+    title: "Categorie",
+    width: 100,
+    dataIndex: "categorie",
+    key: "categorie",
+    fixed: "left",
+  },
+  {
+    title: "Compte Comptable",
+    width: 100,
+    dataIndex: "compte Comptable",
+    key: "compte Comptable",
+    fixed: "left",
+  },
+  {
+    title: "Duree",
+    width: 100,
+    dataIndex: "duree",
+    key: "duree",
+    fixed: "left",
+  },
+  {
+    title: "montant",
+    width: 100,
+    dataIndex: "montant",
+    key: "montant",
+    fixed: "left",
+  },
+  {
+    title: "Date mis en service",
+    width: 100,
+    dataIndex: "date mis en service",
+    key: "date mis en service",
+    fixed: "left",
+  },
+  {
+    title: "Date d acquisition",
+    width: 100,
+    dataIndex: "Date d acquisition",
+    key: "Date d acquisition",
+    fixed: "left",
+  },
+  {
+    title: "Type d amortissement",
+    width: 100,
+    dataIndex: "Type d amortissement",
+    key: "Type d amortissement",
+    fixed: "left",
+  },
 
-  const FicheImmo =() => {
-    const [clients, setClients] = useState([]);
+  {
+    title: "Date d acquisition",
+    width: 100,
+    dataIndex: "categorie",
+    key: "categorie",
+    fixed: "left",
+  },
+  {
+    title: "Action",
+    key: "operation",
+    fixed: "right",
+    width: 100,
+    render: (im) => {
+      return (
+        <button
+          onClick={async () => {
+            await removeImo(im._id);
+            window.location.reload("/client");
+          }}
+        >
+          supprimer
+        </button>
+      );
+    },
+  },
+];
 
-    useEffect(() => {
-      getAllClients()// fetch as usual here!
-      .then(data => {
-        setClients(data.data.result)
-       });
-  
-    }, [])
-      return <Table columns={columns} dataSource={clients} scroll={{ x: 1300 }} />
-  }
-  export default FicheImmo;
+const FicheImmo = () => {
+  const [imo, setImo] = useState([]);
+
+  useEffect(() => {
+    getAllImo() // fetch as usual here!
+      .then((data) => {
+        setImo(data.data.result);
+      });
+  }, []);
+
+  return (
+    <Table
+      columns={columns}
+      dataSource={imo}
+      setImo={setImo}
+      scroll={{ x: 1300 }}
+    />
+  );
+};
+export default FicheImmo;
--- src/components/saisi.js
+++ src/components/saisi.js
@@ -1,6 +1,6 @@
-import { Form, Input, InputNumber, Button, DatePicker, Radio } from 'antd';
-import React from 'react';
-import { createImo } from '../services/imoServices';
+import { Form, Input, InputNumber, Button, DatePicker, Radio } from "antd";
+import React from "react";
+import { createImo } from "../services/imoServices";
 
 const layout = {
   labelCol: { span: 8 },
@@ -9,54 +9,76 @@ const layout = {
 
 /* eslint-disable no-template-curly-in-string */
 const validateMessages = {
-  required: '${label} is required!',
+  required: "${label} is required!",
   types: {
-    email: '${label} is not a valid email!',
-    number: '${label} is not a valid number!',
+    email: "${label} is not a valid email!",
+    number: "${label} is not a valid number!",
   },
   number: {
-    range: '${label} must be between ${min} and ${max}',
+    range: "${label} must be between ${min} and ${max}",
   },
 };
 /* eslint-enable no-template-curly-in-string */
 
 const Saisi = () => {
-   
   const onFinish = async (values) => {
     console.log(values);
-   await  createImo(values.user)
+    await createImo(values.user);
   };
 
   return (
-    <Form {...layout} name="nest-messages" onFinish={onFinish} validateMessages={validateMessages}>
-      
-      <Form.Item name={['user', 'designation']} label="Designation:" rules={[{ type: 'number', min: 0, max: 99 }]}>
+    <Form
+      {...layout}
+      name="nest-messages"
+      onFinish={onFinish}
+      validateMessages={validateMessages}
+    >
+      <Form.Item
+        name={["user", "designation"]}
+        label="Designation:"
+        rules={[{ type: "number", min: 0, max: 99 }]}
+      >
         <InputNumber />
       </Form.Item>
-      <Form.Item name={['user', 'categorie']} label="Categorie:" rules={[{ type: 'string', min: 0, max: 99 }]}>
+      <Form.Item
+        name={["user", "categorie"]}
+        label="Categorie:"
+        rules={[{ type: "string", min: 0, max: 99 }]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        name={["user", "compte_comptable"]}
+        label="Compte Comptable:"
+        rules={[{ type: "number", min: 0, max: 99 }]}
+      >
         <InputNumber />
       </Form.Item>
-      <Form.Item name={['user', 'compte comptable']} label="Compte Comptable:" rules={[{ type: 'number', min: 0, max: 99 }]}>
+      <Form.Item
+        name={["user", "duree"]}
+        label="Durée:"
+        rules={[{ type: "number", min: 0, max: 99 }]}
+      >
         <InputNumber />
       </Form.Item>
-      <Form.Item name={['user', 'duree']} label="Durée:" rules={[{ type: 'number', min: 0, max: 99 }]}>
-        <InputNumber />
-      </Form.Item>
-      <Form.Item name={['user', 'montant']} label="Montant" rules={[{ type: 'number', min: 0, max: 99 }]}>
+      <Form.Item
+        name={["user", "montant"]}
+        label="Montant"
+        rules={[{ type: "number", min: 0, max: 99 }]}
+      >
         <InputNumber />
       </Form.Item>
       <Form.Item label="Date d'acquisition:">
-          <DatePicker />
-        </Form.Item>
-        <Form.Item label="Date mis en service">
-          <DatePicker />
-        </Form.Item>
-       
-        <Form.Item name="radio-group" label="Type d'amortissement">
+        <DatePicker />
+      </Form.Item>
+      <Form.Item label="Date mis en service">
+        <DatePicker />
+      </Form.Item>
+
+      <Form.Item name="radio-group" label="Type d'amortissement">
         <Radio.Group>
           <Radio value="a">Lineaire</Radio>
           <Radio value="b">Dégressif</Radio>
-          
         </Radio.Group>
       </Form.Item>
 
@@ -66,7 +88,7 @@ const Saisi = () => {
         </Button>
       </Form.Item>
     </Form>
-  )
+  );
 };
 
 export default Saisi;
